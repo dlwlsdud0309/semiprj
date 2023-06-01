f877daba0f451e3abc3a5f9ca5d4fc8157371873
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-01 16:29:36 +0900

    버튼 설정 적용 없이 '돌아가기', '결제하기'만 보이는 상황

--- src/com/tech/semiprj/OrderConfirmation.java
+++ src/com/tech/semiprj/OrderConfirmation.java
@@ -8,6 +8,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map.Entry;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,6 +20,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import com.tech.semiprj.CoffeeOrder;
+import com.tech.semiprj.CompletePayment;
 
 public class OrderConfirmation extends JFrame {
 	
@@ -25,6 +30,7 @@ public class OrderConfirmation extends JFrame {
 
 	CoffeeHot ch;
 	CoffeeOrder co;
+	CompletePayment cp;
 
 	JButton[] oc_buttons;
 	JPanel[] oc_panels;
@@ -67,29 +73,112 @@ public class OrderConfirmation extends JFrame {
 				add(checkLabel);
 			}else if(oc_strPanel[i]=="centerP") {
 				add(totalAmount);
+			}else if(oc_strPanel[i]=="downP") {
+				
+				for (int j = 0; j < oc_strButton.length; j++) {
+					oc_buttons[j] = new JButton(oc_strButton[j]);
+					oc_buttons[j].setPreferredSize(new Dimension(100, 50));
+					oc_buttons[j].setFont(new Font("맑은고딕",Font.BOLD,15));
+					oc_buttons[j].setForeground(Color.white);
+					oc_buttons[j].setBorderPainted(false);
+					
+					if(oc_strButton[j]=="결제하기") {
+						setBackground(new Color(0,0,0));
+						oc_buttons[j].addActionListener(new ActionListener() {
+							
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								co.ordermap.clear();
+								new CoffeeOrder(); //초기화하고 처음화면으로 돌아가기
+								dispose();
+							}
+						});
+					}else {
+						setBackground(new Color(243,156,18));
+						new ActionListener() {
+							
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								System.out.println("결제하기 클릭");
+								
+								String orderStr="";
+								for (Entry<String,Integer> entry : co.ordermap.entrySet()) {
+							         System.out.println("key : "+entry.getKey()+", value : "+entry.getValue());
+							         
+							         if(entry.getKey()=="(HOT)아메리카노") {
+							            orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 1500원\t    \t총액 : "+(1500*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="(HOT)카 페 모 카") {
+							            orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 3700원\t    \t총액 : "+(3700*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="(HOT)바닐라라떼") {
+							            orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 3700원\t    \t총액 : "+(3700*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="(HOT)티라미수라떼") {
+							            orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 3900원\t    \t총액 : "+(3900*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="(ICE)아메리카노") {
+							            orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 2000원\t    \t총액 : "+(2000*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="(ICE)헤이즐넛라떼") {
+							            orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 3200원\t    \t총액 : "+(3200*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="(ICE)콜드브루라떼") {
+							            orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 3800원\t    \t총액 : "+(3800*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="허니브레드\t") {
+								        orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 4500원\t    \t총액 : "+(4500*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="말차 스모어 쿠키") {
+								        orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 2900원\t    \t총액 : "+(2900*entry.getValue())+"원    "+"\n";
+							         }
+							         if(entry.getKey()=="아이스크림크로플") {
+								        orderStr=orderStr+entry.getKey()+"  "+entry.getValue()+"개\t 3000원\t    \t총액 : "+(3000*entry.getValue())+"원    "+"\n";
+							         }
+								}
+								cn=makeConnection();
+								Statement stmt;
+								String receiptSql="insert into coffeereceipt values(order_SEQ.NEXTVAL,'"+orderStr+"')";
+								try {
+									stmt=cn.createStatement();
+									stmt.executeUpdate(receiptSql);
+								} catch (SQLException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								
+								
+								dispose();
+								
+								ResultSet rs;
+								
+								String JFwaitingNumbersql = "select max(order_no) from coffeereceipt";
+								try {
+									stmt = cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+//									int no=stmt.executeUpdate("select * from coffeereceipt");
+									rs = stmt.executeQuery(JFwaitingNumbersql);
+									rs.next();
+									cp.JFwaitingNumber.setText("" + rs.getInt("max(order_no)"));
+								} catch (SQLException e1) {
+									e1.printStackTrace();
+								}
+							}
+						};
+					}
+					oc_panels[i].add(oc_buttons[j]);
+				}
+			}
+			
+			if(oc_strPanel[i]=="topP") {
+				add(oc_panels[i],BorderLayout.NORTH);
+			}else if(oc_strPanel[i]=="centerP") {
+				add(oc_panels[i],BorderLayout.CENTER);
+			}else if(oc_strPanel[i]=="downP") {
+				add(oc_panels[i],BorderLayout.SOUTH);
 			}
 		}
 		
-		for (int i = 0; i < oc_strButton.length; i++) {
-			oc_buttons[i] = new JButton(oc_strButton[i]);
-			oc_buttons[i].setPreferredSize(new Dimension(100, 50));
-			oc_buttons[i].setFont(new Font("맑은고딕",Font.BOLD,15));
-			oc_buttons[i].setForeground(Color.white);
-			oc_buttons[i].setBorderPainted(false);
-//			oc_buttons[i].addActionListener(this);
-			
-			if(oc_strButton[i]=="결제하기") {
-				oc_buttons[i].addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						co.ordermap.clear();
-						new CoffeeOrder(); //초기화하고 처음화면으로 돌아가기
-						dispose();
-					}
-				});
-			}
-		}
+		
 
 		
 		setVisible(true);
